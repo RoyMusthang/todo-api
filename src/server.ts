3ba2c6ccb8f232aca433e8fc08e11b7a7ecf440d
Josué Lobo <darksiders20005@gmail.com>
2022-12-09 17:19:24 -0300

    feat: implement mongose connection with api

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import api from './api'
 import vars from './vars'
 
-api.listen(vars.api.port, () => {
-  console.log(`running on port ${vars.api.port}`)
-})
- 
+import mongoose from './db'
+
+mongoose.authenticate()
+  .then(() => {
+    api.listen(vars.api.port, () => {
+      console.log(`running on port ${vars.api.port}`)
+    })
+  })
+  .catch((error: Error) => {
+    console.error(error)
+    process.exit(1)
+  })
